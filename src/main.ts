d0e27f15bdfcfb7bb64587097c8e57c30b34d4be
Joseph Azevedo <joseph.az@gatech.edu>
2020-05-25 05:24:08 -0400

    fix: remove debug output of job

--- src/main.ts
+++ src/main.ts
@@ -128,7 +128,6 @@ async function getJob(
     ...repo,
     run_id: runId,
   });
-  core.debug(JSON.stringify(data));
   const foundJobs = data.jobs.filter((job) => job.name === jobName);
   if (foundJobs.length === 0) {
     core.warning(
